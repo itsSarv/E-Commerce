0f137f2f5d45b1c272257071ceeed3525d07c101
saurav tiwari <sauravtiwari145@gmail.com>
2024-05-10 22:53:27 +0545

    adding Links

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import './Navbar.css'
 
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     const [menu, setMenu] = useState("shop");
@@ -15,15 +16,15 @@ const Navbar = () => {
             <p>SHOPPER</p>
         </div>
         <ul className="nav-menu">
-            <li onClick={()=>{setMenu("shop")}}>Shop{menu==="shop"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("mens")}}>Men{menu==="mens"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("womens")}}>Women{menu==="womens"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("kids")}}>Kids{menu==="kids"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("shop")}}><Link style={{textDecoration:'none'}} to ='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("mens")}}><Link style={{textDecoration:'none'}}  to ='/mens'>Men</Link>{menu==="mens"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("womens")}}><Link style={{textDecoration:'none'}}  to ='/womens'>Women</Link>{menu==="womens"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("kids")}}><Link style={{textDecoration:'none'}}  to ='/kids'>Kids</Link>{menu==="kids"?<hr/>:<></>}</li>
         </ul>
         <div className='nav-login-cart'>
-            <button>Login</button>
-            <img src={cart_icon} alt="Cart Icon" />
-            <div className="nav-cart-count">0</div>
+           <Link style={{textDecoration:'none'}} to='/login'><button>Login</button></Link> 
+           <Link style={{textDecoration:'none'}}  to='/cart'>  <img src={cart_icon} alt="Cart Icon" /></Link> 
+          <div className="nav-cart-count">0</div>
         </div>
     </div>
   )
