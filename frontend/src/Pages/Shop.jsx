f5df56211de3e5af2e45e3c358534a826aad9368
saurav tiwari <sauravtiwari145@gmail.com>
2024-05-16 19:57:40 +0545

    added popular items in home page

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Hero  from '../Components/Hero/Hero'
-
+import Popular from '../Components/popular/popular'
 const Shop = () => {
   return (
     <div>
       <Hero/>
+      <Popular/>
     </div>
   )
 }
