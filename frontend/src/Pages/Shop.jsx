418b96aa1db53057eea5ef20f077c96e6a35cf4f
saurav tiwari <sauravtiwari145@gmail.com>
2024-05-17 18:10:25 +0545

    offers section created

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Hero  from '../Components/Hero/Hero'
 import Popular from '../Components/popular/popular'
+import Offers from '../Components/Offers/offer'
+
 const Shop = () => {
   return (
     <div>
       <Hero/>
       <Popular/>
+      <Offers/>
     </div>
   )
 }
