4df12f7de675236c32bf03fd731a62eeb6027df5
saurav tiwari <sauravtiwari145@gmail.com>
2024-05-19 22:04:18 +0545

    created loginSignup Page and Product Breadcrums Page

--- frontend/src/Pages/loginSignup.jsx
+++ frontend/src/Pages/loginSignup.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import './CSS/loginSignup.css'
 
 const loginSignup = () => {
   return (
-    <div>loginSignup</div>
+    <div className='loginSignup'>
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='Your Name' />
+          <input type="email" placeholder='Email Address' />
+          <input type="password" placeholder='Password' />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">Already have an account? <span>Login here</span></p>
+        <div className="login-signup-agree">
+          <input type="checkbox" name='' id='' />
+          <p>By continuing, i agree to the terms of use & privacy policy.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
